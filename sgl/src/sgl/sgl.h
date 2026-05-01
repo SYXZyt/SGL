2301fe0598c50953fc26c31af9f468bc74b67425
Jake Milner <49202777+SYXZyt@users.noreply.github.com>
2026-05-01 11:20:49 +0100

    Added some macros

--- sgl/src/sgl/sgl.h
+++ sgl/src/sgl/sgl.h
@@ -11,6 +11,12 @@
     #define SGL_API
 #endif
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+#endif
+
 using uint8 = uint8_t;
 using uint16 = uint16_t;
 using uint32 = uint32_t;
@@ -25,3 +31,18 @@ using byte = uint8_t;
 using word = uint16_t;
 using dword = uint32_t;
 using qword = uint64_t;
+
+#define SGL_ENUM_FLAG(i) = (1 << (i))
+
+#define sglFuncPtr(returnType, ...) returnType(*)(__VA_ARGS__)
+
+#define cbuffer struct alignas(16)
+
+namespace sgl
+{
+    // If we use singletons, then this can be used to get any singleton
+    template <typename T>
+    T& Get() {
+        return T::Get();
+    }
+}
