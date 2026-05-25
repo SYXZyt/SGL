77f0bf0cfe325cfbb27d195a27b9e26bbd1a0ec8
Jake Milner <49202777+SYXZyt@users.noreply.github.com>
2026-05-25 09:20:17 +0100

    Added resize callback

--- sgl/src/sgl/Window.h
+++ sgl/src/sgl/Window.h
@@ -5,8 +5,6 @@
 
 SGL_BEGIN
 
-#define SGL_WINDOW_RESIZE_MAX_CALLBACK 32
-
 typedef struct sgl_EngineConfig sgl_sealed
 {
     const char* title;
@@ -17,19 +15,24 @@ typedef struct sgl_EngineConfig sgl_sealed
 
 SGL_API extern const sgl_EngineConfig sgl_EngineConfig_Default;
 
-typedef sglFuncPtr(sgl_Window_Resize_Callback, void, struct sgl_Window*);
+typedef sglFuncPtr(sgl_Window_Resize_Callback_ptr, void, struct sgl_Window*, sgl_Vec2i, void*);
+
+typedef struct sgl_Window_Resize_Callback sgl_sealed
+{
+    sgl_Window_Resize_Callback_ptr ptr;
+    void* userdata;
+} sgl_Window_Resize_Callback;
 
 typedef struct sgl_Window
 {
-    sgl_Window_Resize_Callback resizeCallbacks[SGL_WINDOW_RESIZE_MAX_CALLBACK];
-    size_t resizeCallbackCount;
-
     sgl_EngineConfig cfg;
     sgl_Vec2i screenSize;
     sgl_Vec2i halfScreenSize;
 
     SDL_Window* window;
     SDL_GLContext glContext;
+
+    void* callbacks; // std::vector<sgl_Window_Resize_Callback>
 
     bool wantsClose;
 
@@ -39,5 +42,9 @@ SGL_API sgl_Window* sgl_Window_Create(sgl_EngineConfig cfg);
 SGL_API extern void sgl_Window_Destroy(sgl_Window* window);
 
 SGL_API extern void sgl_Window_PollEvents(sgl_Window* window);
+SGL_API extern bool sgl_Window_WantClose(sgl_Window* window);
+
+SGL_API extern void sgl_Window_RegisterResize(sgl_Window* window, sgl_Window_Resize_Callback_ptr funcptr, void* userdata);
+SGL_API extern void sgl_Window_DeregisterResize(sgl_Window* window, sgl_Window_Resize_Callback_ptr funcptr);
 
 SGL_END
