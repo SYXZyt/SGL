9550fe413dfc29cfee76997ccfeb53a2f2657f42
Jake Milner <49202777+SYXZyt@users.noreply.github.com>
2026-08-05 16:41:10 +0100

    Updated version

--- SGL/src/SGL/Version.cpp
+++ SGL/src/SGL/Version.cpp
@@ -3,7 +3,7 @@
 
 const char* SGL_VERSION_ID = "Beta";
 uint32 SGL_VERSION_MAJOR = 0;
-uint32 SGL_VERSION_MINOR = 4;
+uint32 SGL_VERSION_MINOR = 5;
 uint32 SGL_VERSION_PATCH = 0;
 static std::string gVersionString = std::string(SGL_VERSION_ID) + " " + std::to_string(SGL_VERSION_MAJOR) + "." + std::to_string(SGL_VERSION_MINOR) + "." + std::to_string(SGL_VERSION_PATCH);
 
