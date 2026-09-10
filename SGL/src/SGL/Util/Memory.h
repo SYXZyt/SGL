6e2a10a1bccef956dcc1e331610240d8c7679df3
Jake Milner <49202777+SYXZyt@users.noreply.github.com>
2026-09-10 23:39:27 +0100

    Memory functions are now correctly static

--- SGL/src/SGL/Util/Memory.h
+++ SGL/src/SGL/Util/Memory.h
@@ -129,15 +129,15 @@ namespace sgl
             ptr = nullptr;
         }
 
-        void* Malloc(size_t size) {
+        static void* Malloc(size_t size) {
             return sgl_Malloc(size);
         }
 
-        void Free(void* ptr) {
+        static void Free(void* ptr) {
             sgl_Free(ptr);
         }
 
-        void* Realloc(void* ptr, size_t newSize) {
+        static void* Realloc(void* ptr, size_t newSize) {
             return sgl_Realloc(ptr, newSize);
         }
     };
